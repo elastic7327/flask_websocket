7430686d7643d23edfea79be4783b7f871203e2f
dan <elastic7327@gmail.com>
2018-04-04 11:40:46 +0900

    temp sa

--- src/flaskr.py
+++ src/flaskr.py
@@ -17,6 +17,51 @@ def on_connect():
                      if h not in ['Host', 'Content-Type', 'Content-Length']}))
 
 
+@socketio.on('disconnect')
+def on_disconnect():
+
+
+global disconnected
+disconnected = '/'
+
+
+@socketio.on('connect', namespace='/test')
+def on_connect_test():
+
+
+send('connected-test')
+send(json.dumps(dict(request.args)))
+send(json.dumps({h: request.headers[h] for h in request.headers.keys()
+                 if h not in ['Host', 'Content-Type', 'Content-Length']}))
+
+
+@socketio.on('disconnect', namespace='/test')
+def on_disconnect_test():
+    global disconnected
+    disconnected = '/test'
+
+
+@socketio.on('message')
+def on_message(message):
+    send(message)
+    if message == 'test session':
+        session['a'] = 'b'
+    if message not in "test noackargs":
+        return message
+
+
+@socketio.on('json')
+def on_json(data):
+    send(data, json=True, broadcast=True)
+    if not data.get('noackargs'):
+        return data
+
+
+@socketio.on('message', namespace='/test')
+def on_message_test(message):
+    send(message)
+
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
