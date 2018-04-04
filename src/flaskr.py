dbb53cb4b1276053fb2f70040776af6701dcfc31
dan <elastic7327@gmail.com>
2018-04-04 09:15:17 +0900

    added

--- src/flaskr.py
+++ src/flaskr.py
@@ -1,5 +1,20 @@
-from flask import Flask
+from flask import Flask, session, request, json as flask_json
+from flask_socketio import SocketIO, send, emit, join_room, leave_room, \
+    Namespace
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'this-is-secret'
+socketio = SocketIO(app)
+
+disconnected = None
+
+
+@socketio.on('connect')
+def on_connect():
+    send('connected')
+    send(json.dumps(dict(request.args)))
+    send(json.dumps({h: request.headers[h] for h in request.headers.keys()
+                     if h not in ['Host', 'Content-Type', 'Content-Length']}))
 
 
 @app.route('/')
